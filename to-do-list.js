ec16d7a6ad12297ca36014dfbda121b8199ea2e3
mohaproprog <mohaproprog@gmail.com>
2025-07-08 23:41:43 +0100

    added lists and value

---
+++ to-do-list.js
@@ -0,0 +1,70 @@
+const inputBox = document.getElementById("inputbox");
+const listContainer = document.getElementById("list-container");
+function addTask(){
+    if (inputBox.value === ""){
+        alert("please Add Any Text");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }inputBox.value ="";
+}
+
+listContainer.addEventListener("click",e=>{
+    if (e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+
+    }
+    else if (e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
