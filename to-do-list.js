2205c49c074e7de34d9871e87020d80f4329486f
mohaproprog <mohaproprog@gmail.com>
2025-07-09 15:38:46 +0100

    finished the proj

--- to-do-list.js
+++ to-do-list.js
@@ -8,22 +8,33 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        saveData();
         span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
+        saveData();
     }inputBox.value ="";
+    saveData();
 }
 
 listContainer.addEventListener("click",e=>{
     if (e.target.tagName==="LI"){
         e.target.classList.toggle("checked");
-
+        saveData();
     }
     else if (e.target.tagName==="SPAN"){
         e.target.parentElement.remove();
+        saveData();
     }
-})
+},false);
 
+function saveData (){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+function showData(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showData();
 
 
 
